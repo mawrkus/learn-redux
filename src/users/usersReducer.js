e085b3318848fb1a58af7e66cf7374c99c4838a7
Marc Mignonsin <marc.mignonsin@softonic.com>
2019-01-27 11:34:03 +0100

    Normalized state

--- src/users/usersReducer.js
+++ src/users/usersReducer.js
@@ -1,11 +1,11 @@
 const { ACTIONS_USERS } = require('./usersActions');
 
-const usersReducer = (state = { list: [], current: {} }, action) => {
+const usersReducer = (state = { all: {}, current: {} }, action) => {
   switch (action.type) {
     case ACTIONS_USERS.UPDATE_USERS_LIST:
       return {
         ...state,
-        list: action.payload.users,
+        all: action.payload.users.reduce((byId, user) => ({ ...byId, [user.id]: user }), {}),
       };
 
     case ACTIONS_USERS.UPDATE_USER:
